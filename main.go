fd068d2cd16ec498730d80848db58684c220ec84
Allyson-English <allyson.english@gmail.com>
2023-08-10 09:14:14 -0600

    wip

--- main.go
+++ main.go
@@ -15,6 +15,59 @@ import (
 )
 
 func main() {
+	chunkedUpload()
+}
+
+func chunkedUpload() {
+	settings, err := shared.LoadConfig[struct {
+		PrivateKey string `yaml:"PRIVATE_KEY"`
+	}]("settings.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	signer, err := bundlr.NewEthereumSigner("0x" + settings.PrivateKey)
+	fmt.Println(crypto.PubkeyToAddress(signer.PrivateKey.PublicKey))
+
+	dataItem := bundlr.BundleItem{
+		Data: arweave.Base64String([]byte("abc")),
+		Tags: bundlr.Tags{
+			bundlr.Tag{Name: "Content-Type", Value: "text"},
+		},
+	}
+
+	err = dataItem.Sign(signer)
+	if err != nil {
+		panic(err)
+	}
+
+	reader, err := dataItem.Reader()
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+
+	responseBody := bytes.NewBuffer(body)
+
+	resp, err := http.Post("https://devnet.bundlr.network/chunks/matic/mKDCoOBbHGhM3irYXLGfqrNCAsJEzkw9br5pGfQgtbU", "application/octet-stream", responseBody)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	sb := string(body)
+	log.Printf(sb)
+}
+
+func singleFileUpload() {
 	// ctx := context.Background()
 	settings, err := shared.LoadConfig[struct {
 		PrivateKey string `yaml:"PRIVATE_KEY"`
@@ -34,7 +87,7 @@ func main() {
 	// })
 
 	dataItem := bundlr.BundleItem{
-		Data: arweave.Base64String([]byte(":)")),
+		Data: arweave.Base64String([]byte(":)2")),
 		Tags: bundlr.Tags{
 			bundlr.Tag{Name: "Content-Type", Value: "text"},
 		},
